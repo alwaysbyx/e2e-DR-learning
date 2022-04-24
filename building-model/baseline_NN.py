e920bbcf00b6dc36c10a5b5eccb93f641b6161b8
alwaysbyx <496127893@qq.com>
2022-04-23 20:14:06 -0700

    Update baseline_NN.py

--- building-model/baseline_NN.py
+++ building-model/baseline_NN.py
@@ -1,76 +1,90 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import time
+import argparse
+import scipy.io as scio
+from scipy.io import savemat
 from utils import solve
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+
+def MLP():
+    model = Sequential()
+    model.add(Dense(64, input_dim=48))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(48))
+    model.compile(optimizer='adam',loss='mean_squared_error')
+    return model
 
 
-class Net(nn.Module):
+def RNNmodel():
+    model = Sequential()
+    model.add(LSTM(units=50,return_sequences=True,input_shape=(48, 1)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50,return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50,return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=48))
+    model.compile(optimizer='adam',loss='mean_squared_error')
+    return model
 
-    def __init__(self, d0=24*2, d1 = 24):
-        super(Net, self).__init__()
+def generate_dataset(N,  T=24):
+    np.random.seed(0)
+    amb_data = np.array(pd.read_excel('dataset/input_data_pool.xlsx',sheet_name='theta_amb')['theta_amb'])
+    price_data = np.array(pd.read_excel('dataset/input_data_pool.xlsx',sheet_name='price')['price'])
 
-        self.fc1 = nn.Linear(d0, d1)
-        self.fc2 = nn.Linear(d1, d1)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        return x
-
-
-def train(iters):
-    layer = Net()
+    C_th = 10 * np.random.uniform(0.9,1.1)
+    R_th = 2 * np.random.uniform(0.9,1.1)
+    P_n = 5 * np.random.uniform(0.9,1.1)
+    eta = 2.5 * np.random.uniform(0.9,1.1)
+    theta_r = 20 * np.random.uniform(0.9,1.1)
+    Delta = np.random.uniform(0.9,1.1)
     
-    results = []
-    show = True
-    optimizer = torch.optim.Adam(layer.parameters(), lr=0.1)
-    for i in range(iters):
-        torch.manual_seed(1)
-        np.random.seed(1)
-               
-        pred = layer(input_tensor)        
-        loss = nn.MSELoss()(output_tensor ,pred)
-        
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        
-        results.append(loss.item())
-        if i % 100==0: print("(iter %d) loss: %g " % (i, results[-1]))
-        #if i % 100 == 1: print(variables)
-        if i == 50:
-            optimizer.param_groups[0]["lr"] = 0.05
-        if show:
-            im = plt.plot(results,color='gray')
-            anno = plt.annotate(f'step:{i}\n loss={loss}', xy=(0.85, 0.9), xycoords='axes fraction',color='black')
-            plt.axis("equal")
-            plt.pause(0.001)
-            anno.remove()
-        
-    return layer
+    pn_value = 0.02 # you can change it as you like
+    a1_value = round(1 - 1/(R_th*C_th),4)
+    a2_value = eta*R_th
+    a3_value = round((1-a1_value)*a2_value,6)
+    max_theta = round(theta_r + Delta,3)
+    min_theta = round(theta_r - Delta,3)
+    max_power = round(P_n,3)
+    params = {'pn':pn_value, 'a1':a1_value,  'a3': a3_value,
+          'max_theta':max_theta, 'min_theta':min_theta, 'max_power':max_power}
+
+    theta_0 = 35.0
+    
+    p,theta,y = solve(price_data[:N*T], amb_data[:N*T], N*T, pn_value, a1_value, a3_value, max_theta, min_theta, max_power, theta_0)
+
+    price = np.array(price_data[:N*T]).reshape(N,T)
+    amb = np.array(price_data[:N*T]).reshape(N,T)
+    x = np.concatenate([price, amb],axis=1)
+
+    p = np.array(p).reshape(N,T)
+    theta = np.array(theta).reshape(N,T)
+    dr = np.concatenate([p, theta],axis=1)
+    return x, dr
+
 
 if __name__ == '__main__':
-    amb_data = np.array(pd.read_excel('input_data_pool.xlsx',sheet_name='theta_amb')['theta_amb'])
-    price_data = np.array(pd.read_excel('input_data_pool.xlsx',sheet_name='price')['price'])
-
-    T = 24*5
-    price = price_data[:T]
-    amb = amb_data[:T]
-    variable = {'pn': 0.02, 'a1': 0.9521839137854551, 'a2': 5.598830495762315, 'a3': 0.2677141616859941, 'max_theta': 20.539969677000002, 'min_theta': 18.670106323, 'max_power': 5.38}
-    pn_value = variable['pn']
-    a1_value = variable['a1']
-    a2_value = variable['a2']
-    max_theta = variable['max_theta']
-    min_theta = variable['min_theta']
-    max_power = variable['max_power']
-    theta_0 = 21.64671372
-    true = solve(price, amb, T, pn_value, a1_value, a2_value, max_theta, min_theta, max_power,theta_0, tensor=True)
-
-    input_price = torch.tensor(price_data[:24*5]).reshape(5,24)
-    input_amb = torch.tensor(amb_data[:24*5]).reshape(5,24)
-    input_tensor = torch.concat([input_price,input_amb],dim=1).to(torch.float32)
-    output_tensor = true[0].reshape(5,24).to(torch.float32)
-
-    model = train(500)
+    X,dr = generate_dataset(65)
+    N = 50
+    train_price = X[1:1+N]
+    train_dr = dr[1:1+N]
+    mlp = MLP()
+    mlp.fit(train_price,train_dr, epochs=500,batch_size=2)
+    end = time.time()
